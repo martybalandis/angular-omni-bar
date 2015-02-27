6257d5642dd8f5cc922401d462d061a9775e5635
Marty Balandis <m.balandisx@gmail.com>
2015-02-27 22:44:47 +0000

    Cleaned up
    Wrapped in function, added strict more, lint compatible, removed empty spaces, added missing semi-colons, fixed other common programming mistakes such as use of typeof(x) === "undefined" (should be x === undefined), removed unused parameters, etc.

--- isteven-omni-bar.js
+++ isteven-omni-bar.js
@@ -1,153 +1,124 @@
-/* 
+/*
  * AngularJS Omni Bar Directive
- * 
+ *
  * Creates a simple bar to display a comparison of sub-value over the total-value.
  *
- * Project started on: Thu, 21 Aug 2014 - 15:30:31 
- * Current version: 1.0.1
- * 
+ * Project started on: Thu, 21 Aug 2014 - 15:30:31
+ * Current version: 1.0.1 (cleaner version by Marty Balandis 27/02/2015)
+ *
  * Released under the MIT License
  * --------------------------------------------------------------------------------
  * The MIT License (MIT)
  *
  * Copyright (c) 2014 Ignatius Steven (https://github.com/isteven)
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy 
- * of this software and associated documentation files (the "Software"), to deal 
- * in the Software without restriction, including without limitation the rights 
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
- * copies of the Software, and to permit persons to whom the Software is 
- * furnished to do so, subject to the following conditions: 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be included in all 
+ * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  * --------------------------------------------------------------------------------
  */
 
-angular.module( 'isteven-omni-bar', ['ng'] ).directive( 'istevenOmniBar' , [ '$timeout', function ( $timeout ) {
-    return {
-        restrict: 
-            'AE',
+(function () {
+    'use strict';
+    /*global angular */
+    angular.module('isteven-omni-bar', ['ng']).directive('istevenOmniBar', ['$timeout', function ($timeout) {
 
-        replace: 
-            true,
-
-        transclude: 
-            true,
-
-        scope: 
-        {   
-            // bars
-            currentValue    : '=',
-            currentStyle    : '=',     
-            maxValue        : '=',            
-            maxStyle        : '=',
-
-            // bar container
-            containerStyle  : '=',
-
-            // callbacks
-            onClick         : '&'
-        },
-
-        template: 
-            '<div class="containerBar">' +       
-                '<div class="maxValue" ng-style="maxStyle">' +                                                         
-                    '<div class="currentValue" ng-style="currentStyle">' + 
-                    '</div>' +                        
-                    '<span ng-transclude class="infoText"></span>' +
+        return {
+            restrict: 'AE',
+            replace: true,
+            transclude: true,
+            scope: {
+                // bars
+                currentValue: '=',
+                currentStyle: '=',
+                maxValue: '=',
+                maxStyle: '=',
+                // bar container
+                containerStyle: '=',
+                // callbacks
+                onClick: '&'
+            },
+            template: '<div class="containerBar">' +
+                '<div class="maxValue" ng-style="maxStyle">' +
+                '<div class="currentValue" ng-style="currentStyle">' +
                 '</div>' +
-            '</div>',
-
-        link: function ( $scope, element, attrs ) {               
-
-            var currentValueBar = element.children().children()[ 0 ];
-            var maxValueBar     = element.children()[ 0 ];
-            var containerBar    = element;
-
-            $scope.drawBar = function( currentVal, maxVal ) {       
-                
-                // if max-value is not specified, we use the current value as the width %
-                // else we calculate using the formula
-                if ( 
-                    typeof attrs.maxValue === 'undefined' || 
-                    attrs.maxValue === null || 
-                    typeof maxVal === 'undefined' || 
-                    maxVal === null 
-                ) {
-                    var calculatedWidth = currentVal; 
-                }
-                // Just to make sure it's a number
-                else if ( maxVal * 1 === 0 ) {
-                    var calculatedWidth = 100;
-                }
-                else {
-                    var calculatedWidth = ( currentVal / maxVal ) * 100;   
-                }
-
-                // set the width here.. just plain ol' javascript 
-                currentValueBar.setAttribute( 'style', 'width:' + calculatedWidth + '%' );
-
-                // Somehow ng-style is removed after we change the width, so to make it persistant:
-                if ( typeof $scope.currentStyle !== 'undefined' ) {
-                    angular.element( currentValueBar ).css( $scope.currentStyle );
-                }
-            
-                if ( typeof $scope.maxStyle !== 'undefined' ) {
-                    angular.element( maxValueBar ).css( $scope.maxStyle );
-                }
-                
-                if ( typeof $scope.containerStyle  !== 'undefined' ) {
-                    angular.element( containerBar ).css( $scope.containerStyle );
-                }
+                '<span ng-transclude class="infoText"></span>' +
+                '</div>' +
+                '</div>',
+            link: function ($scope, element, attrs) {
+                var currentValueBar = element.children().children()[0],
+                    maxValueBar = element.children()[0],
+                    containerBar = element,
+                    calculatedWidth = 0;
+                $scope.drawBar = function (currentVal, maxVal) {
+                    // if max-value is not specified, we use the current value as the width %
+                    // else we calculate using the formula
+                    if (attrs.maxValue === undefined || attrs.maxValue === null || maxVal === undefined || maxVal === null) {
+                        calculatedWidth = currentVal;
+                    } else if (Number(maxVal) === 0) { // Just to make sure it's a number
+                        calculatedWidth = 100;
+                    } else {
+                        calculatedWidth = (currentVal / maxVal) * 100;
+                    }
+                    // set the width here.. just plain ol' javascript
+                    currentValueBar.setAttribute('style', 'width:' + calculatedWidth + '%');
+                    // Somehow ng-style is removed after we change the width, so to make it persistant:
+                    if ($scope.currentStyle !== undefined) {
+                        angular.element(currentValueBar).css($scope.currentStyle);
+                    }
+                    if ($scope.maxStyle !== undefined) {
+                        angular.element(maxValueBar).css($scope.maxStyle);
+                    }
+                    if ($scope.containerStyle !== undefined) {
+                        angular.element(containerBar).css($scope.containerStyle);
+                    }
+                };
+                // initial draw (0%) so we can see a nice animation when the current-value is actually loaded by $watch below
+                $scope.drawBar(0, null);
+                // watches.. nothing unusual here.
+                // timeout is used for the loading animation
+                $timeout(function () {
+                    $scope.$watch('currentValue', function (newVal) {
+                        if (newVal !== undefined) {
+                            $scope.drawBar(newVal, $scope.maxValue);
+                        }
+                    });
+                    $scope.$watch('maxValue', function (newVal) {
+                        if (newVal !== undefined) {
+                            $scope.drawBar($scope.currentValue, newVal);
+                        }
+                    });
+                    $scope.$watch('currentStyle', function (newVal) {
+                        if (newVal !== undefined) {
+                            angular.element(currentValueBar).css(newVal);
+                        }
+                    });
+                    $scope.$watch('maxStyle', function (newVal) {
+                        if (newVal !== undefined) {
+                            angular.element(maxValueBar).css(newVal);
+                        }
+                    });
+                    $scope.$watch('containerStyle', function (newVal) {
+                        if (newVal !== undefined) {
+                            angular.element(containerBar).css(newVal);
+                        }
+                    });
+                }, 250);
             }
-
-            // initial draw (0%) so we can see a nice animation when the current-value is actually loaded by $watch below
-            $scope.drawBar( 0 , null );
-
-            // watches.. nothing unusual here.
-            // timeout is used for the loading animation
-            $timeout( function() {            
-                
-                $scope.$watch( 'currentValue' , function( newVal, oldVal ) {
-                    if ( typeof newVal !== 'undefined' ) {
-                        $scope.drawBar( newVal, $scope.maxValue );                    
-                    }
-                });
-
-                $scope.$watch( 'maxValue', function( newVal, oldVal ) {
-                    if ( typeof newVal !== 'undefined' ) {
-                        $scope.drawBar( $scope.currentValue , newVal );                    
-                    }
-                });
-
-                $scope.$watch( 'currentStyle', function( newVal, oldVal ) {   
-                    if ( typeof newVal !== 'undefined' ) {
-                        angular.element( currentValueBar ).css( newVal );
-                    }
-                });
-
-                $scope.$watch( 'maxStyle', function( newVal, oldVal ) {   
-                    if ( typeof newVal !== 'undefined' ) {
-                        angular.element( maxValueBar ).css( newVal );
-                    }
-                });
-                
-                $scope.$watch( 'containerStyle', function( newVal, oldVal ) {   
-                    if ( typeof newVal !== 'undefined' ) {
-                        angular.element( containerBar ).css( newVal );
-                    }
-                });
-
-            }, 250 );
-        }
-    }
-}]);
+        };
+    }]);
+}());
